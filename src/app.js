83b10fd34ebc43fdb710cb4c5ba207751bbc0ca1
Nick66v <136284543+Nick66v@users.noreply.github.com>
2023-06-22 03:28:14 +0000

    added function

--- src/app.js
+++ src/app.js
@@ -37,3 +37,57 @@ let locationh2 = document.getElementById("location")
 locationh2.innerHTML = "I live in the hood now."
 
 console.log(locationh2)
+
+///////////////
+
+// CREATE A FUNCTION
+// is a piece of code, that can run over and over again when called upon
+// EVERY function has a parenthesis ()
+
+// this is an ANONYMOUS function ---- the parenthesis is empty()
+function hello() {
+    console.log( "this is the hello function") 
+}
+
+//we are calling the function here
+hello();
+
+///////
+
+// function with a PARAMETER
+function saywhat(word) {
+console.log("The word you typed is:",word)
+}
+
+saywhat("F dem kids");
+saywhat("Parkus");
+saywhat(36);
+
+/////////
+
+let count = 0;
+
+function counter(){
+    count = count + 1
+    console.log("The button was clicked: ", count)
+    let number = document.getElementById("counts")
+    
+    number.innerHTML = count
+}
+
+
+
+/////// In JavaScript there are EVENTS that we can listen to
+////// what are some events? Click, a scroll, a hover, a type, a key, etc
+
+////FIRST STEP: identify what you want to target
+
+// let button = document.getElementById("button")
+// console.log(button)
+
+/// STEP 2: add a function to the "Onclick" event
+
+// button.onclick = counter;
+console.log(count)
+
+////
